1cf30f55d10753ffa49ba5137d8efa2dd165d822
Stan Haakman <stanhaakman@icloud.com>
2021-04-19 18:42:16 +0200

    Skelet Boek afmaken

--- src/bibliotheek/domain/uitleenables/Boek.java
+++ src/bibliotheek/domain/uitleenables/Boek.java
@@ -3,6 +3,30 @@ package bibliotheek.domain.uitleenables;
 import bibliotheek.domain.Artikelen;
 
 public class Boek implements Artikelen {
+    private final String titel;
+    private final String auteur;
+    private final int publicatieJaar;
+    private boolean isUitgeleend;
+
+    public Boek(String titel, String auteur, int publicatieJaar, boolean isUitgeleend) {
+        this.titel = titel;
+        this.auteur = auteur;
+        this.publicatieJaar = publicatieJaar;
+        this.isUitgeleend = isUitgeleend;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public int getPublicatieJaar() {
+        return publicatieJaar;
+    }
+
     @Override
     public void uitlenen() {
 
