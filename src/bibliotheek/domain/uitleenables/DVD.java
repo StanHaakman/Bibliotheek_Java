adcbfd24ac1d46e48e8339a0fe64ade669b75e12
Stan Haakman <stanhaakman@icloud.com>
2021-04-19 18:46:10 +0200

    Skelet DVD afmaken

--- src/bibliotheek/domain/uitleenables/DVD.java
+++ src/bibliotheek/domain/uitleenables/DVD.java
@@ -3,6 +3,40 @@ package bibliotheek.domain.uitleenables;
 import bibliotheek.domain.Artikelen;
 
 public class DVD implements Artikelen {
+    private final String titel;
+    private final String regisseur;
+    private final int releaseJaar;
+    private boolean isUitgeleend;
+
+    public DVD(String titel, String regisseur, int releaseJaar, boolean isUitgeleend) {
+        this.titel = titel;
+        this.regisseur = regisseur;
+        this.releaseJaar = releaseJaar;
+        this.isUitgeleend = isUitgeleend;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public String getRegisseur() {
+        return regisseur;
+    }
+
+    public int getReleaseJaar() {
+        return releaseJaar;
+    }
+
+    @Override
+    public String toString() {
+        return "DVD{" +
+                "titel='" + titel + '\'' +
+                ", regisseur='" + regisseur + '\'' +
+                ", releaseJaar=" + releaseJaar +
+                ", isUitgeleend=" + isUitgeleend +
+                '}';
+    }
+
     @Override
     public void uitlenen() {
 
